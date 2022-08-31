8c26da5a79c8190ca933fd404ccf8c3c1b015002
Rogerio Albandes <rogerio.albandes@gmail.com>
2022-08-31 18:11:20 -0300

    Adiconado .env

--- listen-new.php
+++ listen-new.php
@@ -3,31 +3,34 @@ error_reporting(E_ALL);
 
 use Albandes\mqtt;
 use Albandes\db;
+use vlucas\phpdotenv;
 
 require("vendor/autoload.php");
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+date_default_timezone_set($_ENV['TIME_ZONE']);
+
 // Use Mosquitto-PHP library
-$client = new Mosquitto\Client('myclient');
-$client->setCredentials('middleware','exehda');
-$client->connect('brokermqtt1.exehda.org');
+$client = new Mosquitto\Client('exehda-client');
+$client->setCredentials($_ENV['BROKER_USERNAME'],$_ENV['BROKER_PASSWORD']);
+$client->connect($_ENV['BROKER_URL']);
+
 
 // Connect database
-$db = new DB('mysql:dbname=exehda;host=10.42.44.200', 'pipeadm', 'qpal10');
+try{
+    $dsn = "mysql:dbname={$_ENV['DB_NAME']};port={$_ENV['DB_PORT']};host={$_ENV['DB_HOSTNAME']}";
+    $db = new DB($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);   
+}catch(\PDOException $e){
+    die("<br>Error connecting to database: " . $e->getMessage() . " File: " . __FILE__ . " Line: " . __LINE__ );
+}
 
-
+// run
 $listen = new mqtt($client, $db);
-$listen->set_debug(true);
+$listen->set_debug($_ENV['DEBUG']);
 $listen->setTopics(['rogerio/#' => 0]);
 $listen->run();
-
-
-
-// $db, $username = NULL, $password = NULL, $host = '127.0.0.1', $port = 3306, $options = []
-
-
-
-echo "\n";
-
 
 
 
